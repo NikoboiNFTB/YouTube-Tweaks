be88fda0a5608229fb7e939f60c76af1a038d029
NikoboiNFTB <github@nikoboi.dev>
2026-02-21 17:57:40 +0200

    Update

--- timestamp-no-scroll.user.js
+++ timestamp-no-scroll.user.js
@@ -2,13 +2,13 @@
 // @name         YouTube - Disable Timestamp Scroll Up
 // @namespace    https://github.com/NikoboiNFTB/YouTube-Tweaks
 // @downloadURL  https://github.com/NikoboiNFTB/YouTube-Tweaks/raw/refs/heads/main/timestamp-no-scroll.user.js
-// @version      1.0
+// @version      1.1
 // @description  Prevents timestamp clicks from scrolling all the way up.
 // @author       Nikoboi
 // @icon         https://www.youtube.com/s/desktop/ab67e92c/img/favicon_144x144.png
 // @match        *://*.youtube.com/watch*
 // @grant        none
-// @run-at       document-start
+// @run-at       document-idle
 // ==/UserScript==
 
 (function () {
